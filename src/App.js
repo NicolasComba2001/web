6684ffa01aa39faf0b1061c24bc311ef0117f584
nc <nc@com.ar>
2022-03-19 01:21:10 -0600

    renegando con rutas

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <>
     <Routes>
       {/*Pagina principal*/}
-      <Route path="https://nicolascomba2001.github.io/web/" element={<SeccionHome></SeccionHome>}>
-      <Route path='Proyectos' element={<SeccionProyecto></SeccionProyecto>}></Route>
+      <Route path="/" element={<SeccionHome></SeccionHome>}>
+      <Route path='/Proyectos' element={<SeccionProyecto></SeccionProyecto>}></Route>
       </Route>
     </Routes>
     <PiePagina></PiePagina>
