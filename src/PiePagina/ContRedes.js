c3a10f7fe6bc2de21dd6ae9f23543f1562d029f9
nc <nc@com.ar>
2022-03-22 00:21:17 -0600

    cambiando el enlace de faceook

--- src/PiePagina/ContRedes.js
+++ src/PiePagina/ContRedes.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ContRedes(){
     return(
         <div className='redescont'>
-            <a href='https://www.facebook.com/nico.comba.50/' target={"_blank"} rel="noreferrer" className='redesitems'><img className='redesitems__img' src="./RedesIMG/facebook.svg" alt="encuentra tu pagina web en nuestra red social, facebook" /></a>
+            <a href='https://www.facebook.com/profile.php?id=100079472563768' target={"_blank"} rel="noreferrer" className='redesitems'><img className='redesitems__img' src="./RedesIMG/facebook.svg" alt="encuentra tu pagina web en nuestra red social, facebook" /></a>
             <a href='https://www.instagram.com/webs_arg/?hl=es-la' target={"_blank"} rel="noreferrer" className='redesitems'><img className='redesitems__img' src="./RedesIMG/instagram.svg" alt="encuentra tu pagina web en nuestra red social, instagram" /></a>
             <a href='https://nicolascomba2001.github.io/Portfolio/' target={"_blank"} rel="noreferrer" className='redesitems'><img className='redesitems__img' src="./RedesIMG/github.svg" alt="encuentra tu pagina web en nuestro portfolio, Github" /></a>
         </div>
